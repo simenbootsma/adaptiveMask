b031fbd914fd152d3d85c5e475af360b44ae86b1
local.la <local.la@UT169842.ad.utwente.nl>
2025-01-31 17:07:23 +0100

    New files

---
+++ move_mouse.py
@@ -0,0 +1,14 @@
+import mouse
+import time
+
+
+move_left = False
+while True:
+    if move_left:
+        mouse.move(0, 500, duration=1)
+        mouse.click()
+    else:
+        mouse.move(1000, 500, duration=1)
+        mouse.click()
+    move_left = not move_left
+    time.sleep(60)
