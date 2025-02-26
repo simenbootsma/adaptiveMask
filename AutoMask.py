41b2518e25799354ea6787433c66fe988a00f17a
simenbootsma <stbootsma@hotmail.com>
2025-02-26 19:13:11 +0100

    Semi-working version with control points

--- AutoMask.py
+++ AutoMask.py
@@ -1,66 +1,136 @@
+import cv2 as cv
 from ice_detection import *
 import matplotlib.pyplot as plt
 from ManualMask import Cylinder
 
 
-BIG_NUMBER = 10**9
-
-
 class AutoMask:
     def __init__(self):
-        self.mask = cv.cvtColor(init_manual_mask(), cv.COLOR_RGB2GRAY)
+        self.display = cv.cvtColor(init_manual_mask(), cv.COLOR_RGB2GRAY)
         self.sensitivity = 0.2  # multiplier for the error
+        self.eta = 50  # target width in camera pixels
+        self.loc = None
+        self.sz_fac = None
 
-    def update(self, img):
-        # y, err_l, err_r = compute_errors_plotting(img)
-        y, err_l, err_r = compute_errors(img)
+    def update(self, img, show=False):
+        # (0) Extract ice and mask from image
+        img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+        mask, ice = find_mask_and_ice(img)
 
-        screen = self.mask.copy()
-        screen[:10, :] = 0
-        mask_edges = find_edges(screen, remove_outside=True)
-        top = np.mean(mask_edges[mask_edges[:, 1] == np.min(mask_edges[:, 1])], axis=0)
-        bottom = np.mean(mask_edges[mask_edges[:, 1] == np.max(mask_edges[:, 1])], axis=0)
-        center_x = int((top[0] + bottom[0]) / 2)
-        top_y = int(top[1])
-        bot_y = int(bottom[1])
+        ice_edges = find_edges(ice, largest_only=True, remove_inside=False)
+        mask_edges = find_edges(mask, remove_outside=True)
 
-        # Convert to screen coordinates
-        L = bot_y - top_y
-        y = (top_y + y * L)
-        err_l = err_l * L
-        err_r = err_r * L
+        if ice_edges is None:
+            print('No ice!')
+            return
 
-        I = np.arange(self.mask.shape[0])
-        el = np.interp(I, y, err_l, left=0, right=0)
-        er = np.interp(I, y, err_r, left=0, right=0)
+        if mask_edges is None:
+            mask_edges = box_edges(mask.shape)
 
-        # Apply
-        left_width = np.sum(self.mask[:, :center_x] == 0, axis=1)
-        right_width = np.sum(self.mask[:, center_x:] == 0, axis=1)
+        mat = mask.copy()
+        cv.fillPoly(mat, [ice_edges], (2, 2, 2))
 
-        # plt.ioff()
-        # plt.figure()
-        # plt.plot(el, I)
-        # plt.plot(er, I)
-        # plt.figure()
-        # plt.plot(left_width, I)
-        # plt.plot(right_width, I)
-        # plt.show()
+        # (1) Obtain target mask
+        not_ice = 1 - (mat - mask)/2
+        dmap_ice = cv.distanceTransform(not_ice.astype(np.uint8), cv.DIST_L2, 5)
+        target = np.where(dmap_ice < self.eta, 0, 1)
+        target_edges = find_edges(target, remove_outside=True)
 
-        new_mask = 255 * np.ones(self.mask.shape, dtype=np.uint8)
-        for i in range(new_mask.shape[0]):
-            new_mask[i, :int(left_width[i] + el[i] * self.sensitivity)] = 0
-            new_mask[i, -int(right_width[i] + er[i] * self.sensitivity):] = 0
+        # (2) Vector of each point on mask edge to target
+        dists = np.zeros(mask_edges.shape)
+        for i in range(len(mask_edges)):
+            d2 = np.sum((mask_edges[i, :] - target_edges)**2, axis=1)
+            dists[i, :] = target_edges[np.argmin(d2), :] - mask_edges[i, :]  # shortest vector from mask to target
 
-        # plt.imshow(new_mask)
-        # plt.show()
-        self.mask = new_mask
+        if self.loc is None or self.sz_fac is None:
+            # (3) Find location of image mask on screen mask TODO: deal with white screen
+            factors = np.linspace(1.5, 2.5, 20)[::-1]
+            values = -1 * np.ones(factors.size)
+            locations = np.zeros((factors.size, 2))
+            for i, fac in enumerate(factors):
+                templ = cv.resize((255*mask).astype(np.uint8), (int(mask.shape[1] / fac), int(mask.shape[0] / fac)))
+                if templ.shape[0] > self.display.shape[0] or templ.shape[1] > self.display.shape[1]:
+                    break
+                res = cv.matchTemplate(255 - self.display, templ, cv.TM_CCOEFF_NORMED)
+                _, max_val, _, max_loc = cv.minMaxLoc(res)
+                values[i] = max_val
+                locations[i, :] = max_loc
+
+            self.loc = locations[np.argmax(values)]
+            self.sz_fac = factors[np.argmax(values)]
+
+        # (4) Define control points
+        ncp = 500  # number of control points
+        disp_edges = find_edges(self.display, remove_outside=True)
+        control_points = disp_edges[::len(disp_edges)//ncp]
+
+        # (5) Find movement for every control point
+        mov = np.zeros(control_points.shape)
+        cnt = np.zeros((control_points.shape[0], 1))
+        sme = mask_edges / self.sz_fac + self.loc  # shifted mask edges
+        for i in range(mask_edges.shape[0]):
+            d = np.sum((sme[i, :] - control_points)**2, axis=1)
+            mov[np.argmin(d), :] += dists[i, :] / self.sz_fac
+            cnt[np.argmin(d)] += 1
+
+        mov = mov / cnt
+        mov[np.isnan(mov)] = 0
+
+        # (6) Move control points
+        control_points = control_points + mov * self.sensitivity
+
+        # (7) Generate new display
+        new_display = np.zeros(self.display.shape)
+        cv.fillPoly(new_display, [control_points.astype(np.int32)], (255, 255, 255))
+        self.display = new_display.astype(np.uint8)
+
+        if show:
+            plt.ioff()
+            if self.loc is None:
+                plt.figure()
+                plt.plot(factors, values, '-o')
+
+            fig, ax = plt.subplots(1, 3)
+            ax[0].imshow(mask)
+            ax[0].plot(mask_edges[:, 0], mask_edges[:, 1], '-r')
+            ax[1].imshow(target)
+            ax[1].plot(target_edges[:, 0], target_edges[:, 1], '-r')
+            ax[2].imshow(mask - target)
+            scat = ax[2].scatter(mask_edges[:, 0], mask_edges[:, 1], 10, np.sqrt(np.sum(dists**2, axis=1)), cmap=plt.get_cmap('Blues'))
+
+            for i in range(0, len(mask_edges), len(mask_edges)//20):
+                ax[2].arrow(mask_edges[i, 0], mask_edges[i, 1], dists[i, 0]/2, dists[i, 1]/2, color='r', width=0.003, head_width=0.1, head_length=0.1)
+
+            ax[0].set_title('mask')
+            ax[1].set_title('target')
+            ax[2].set_title('diff')
+
+            fig, ax = plt.subplots(1, 2)
+            ax[0].imshow(self.display)
+            ax[0].add_artist(plt.Rectangle((self.loc[0], self.loc[1]), width=mask.shape[1]/self.sz_fac, height=mask.shape[0]/self.sz_fac, fc='none', ec='r'))
+            ax[0].plot(control_points[:, 0], control_points[:, 1], '-og', markersize=3)
+            ax[0].plot(sme[:, 0], sme[:, 1], '-m', markersize=3)
+            for i in range(len(control_points)):
+                ax[0].arrow(control_points[i, 0], control_points[i, 1], mov[i, 0], mov[i, 1])
+            ax[0].set_title('display')
+
+            ax[1].imshow(new_display)
+            ax[1].set_title('new display')
+
+            for a in ax:
+                a.tick_params(labelleft=False, labelbottom=False, left=False, bottom=False)
+            plt.show()
 
     def get_img(self):
-        return self.mask
+        return self.display
 
 
 def init_manual_mask():
+    im = np.zeros((2000, 1000, 3), dtype=np.uint8)
+    im[300:1150, 500:900] = 255
+    # cv.circle(im, (500, 500), 400, color=(255, 255, 255), thickness=-1)
+    return im
+
     cyl = Cylinder(resolution=(8256, 5504))
     cyl.transpose()
     cyl.set_center(4000)
@@ -83,175 +153,16 @@ def init_manual_mask():
     return obj.get_img()
 
 
-def compute_errors(img):
-    eta = 400  # target width in camera pixels
-
-    img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-    mask, ice = find_mask_and_ice(img)
-
-    ice_edges = find_edges(ice, largest_only=True, remove_inside=True)
-    mask_edges = find_edges(mask, remove_outside=True)
-
-    if ice_edges is None or mask_edges is None:
-        return None
-
-    mat = np.zeros(ice.shape, dtype=np.uint8)
-    mat = cv.fillPoly(mat, [mask_edges.astype(np.int32)], color=(1, 1, 1))
-    mat = cv.fillPoly(mat, [ice_edges.astype(np.int32)], color=(2, 2, 2))
-
-    top = np.mean(mask_edges[mask_edges[:, 1] == np.min(mask_edges[:, 1])], axis=0)
-    bottom = np.mean(mask_edges[mask_edges[:, 1] == np.max(mask_edges[:, 1])], axis=0)
-    center_x = int((top[0] + bottom[0]) / 2)
-    top_y = int(top[1])
-    bot_y = int(bottom[1])
-
-    left = mat[:, :center_x]
-    right = mat[:, center_x:]
-
-    maskbw_l = np.sum(left < 2, axis=1)  # width of mask black and white area on left side
-    maskbw_r = np.sum(right < 2, axis=1)  # width of mask black and white area on right side
-    maskb_l = np.sum(left == 0, axis=1)  # width of mask black area on left side
-    maskb_r = np.sum(right == 0, axis=1)  # width of mask black area on right side
-    ice_width_l = np.sum(left == 2, axis=1)  # width of ice on the left side
-    ice_width_r = np.sum(right == 2, axis=1)  # width of ice on the right side
-
-    # Add big number to ignore parts where there is no ice in the error computation. Might be a better way.....?
-    maskbw_l[ice_width_l == 0] += BIG_NUMBER
-    maskbw_r[ice_width_r == 0] += BIG_NUMBER
-
-    r_l = np.nan * np.zeros((left.shape[0], 2*eta))
-    r_r = np.nan * np.zeros((right.shape[0], 2*eta))
-
-    r_l[:, 0] = maskbw_l - maskb_l
-    r_r[:, 0] = maskbw_r - maskb_r
-    for i in range(1, eta):
-        r_l[:-i, -i] = np.sqrt((maskbw_l[i:] - maskb_l[:-i])**2 + i**2)
-        r_l[i:, i] = np.sqrt((maskbw_l[:-i] - maskb_l[i:])**2 + i**2)
-
-        r_r[:-i, -i] = np.sqrt((maskbw_r[i:] - maskb_r[:-i])**2 + i**2)
-        r_r[i:, i] = np.sqrt((maskbw_r[:-i] - maskb_r[i:])**2 + i**2)
-
-    error_l = np.nanmin(r_l, axis=1) - eta
-    error_r = np.nanmin(r_r, axis=1) - eta
-
-    # Subtract big number again
-    error_r[error_r > BIG_NUMBER/2] -= BIG_NUMBER - eta
-    error_l[error_l > BIG_NUMBER / 2] -= BIG_NUMBER - eta
-
-    # Normalize coordinates
-    L = (bot_y - top_y)  # length scale (height of the mask)
-    y = (np.arange(mat.shape[0]) - top_y) / L
-    error_l = error_l / L
-    error_r = error_r / L
-    return y, error_l, error_r
+def smoothen_array(y, n):
+    y = [np.nanmean(y[max(0, i - n // 2):min(len(y), i + n // 2)]) for i in range(len(y))]
+    return np.array(y)
 
 
-def compute_errors_plotting(img):
-    plt.ioff()
-    eta = 400  # target width in camera pixels
+def box_edges(shape):
+    return np.array([[0, i] for i in range(shape[0])]                       # left
+                    + [[j, shape[0]-1] for j in range(shape[1])]            # bottom
+                    + [[shape[1]-1, i] for i in range(shape[0])]            # right
+                    + [[j, 0] for j in range(shape[1]-1, -1, -1)]           # top
+                    )
 
-    img = cv.cvtColor(img, cv.COLOR_RGB2GRAY)
-    mask, ice = find_mask_and_ice(img)
-
-    plt.figure()
-    plt.imshow(mask)
-
-    plt.figure()
-    plt.imshow(ice)
-
-    ice_edges = find_edges(ice, largest_only=True, remove_inside=True)
-    mask_edges = find_edges(mask, remove_outside=True)
-
-    mat = np.zeros(ice.shape, dtype=np.uint8)
-    if mask_edges is not None:
-        mat = cv.fillPoly(mat, [mask_edges.astype(np.int32)], color=(1, 1, 1))
-        plt.plot(mask_edges[:, 0], mask_edges[:, 1], 'og')
-    if ice_edges is not None:
-        mat = cv.fillPoly(mat, [ice_edges.astype(np.int32)], color=(2, 2, 2))
-        plt.plot(ice_edges[:, 0], ice_edges[:, 1], 'or')
-
-    top = np.mean(mask_edges[mask_edges[:, 1] == np.min(mask_edges[:, 1])], axis=0)
-    bottom = np.mean(mask_edges[mask_edges[:, 1] == np.max(mask_edges[:, 1])], axis=0)
-    center_x = int((top[0] + bottom[0]) / 2)
-    top_y = int(top[1])
-    bot_y = int(bottom[1])
-
-    plt.figure()
-    plt.imshow(mat)
-
-    plt.plot(top[0], top[1], '^')
-    plt.plot(bottom[0], bottom[1], 'v')
-
-    left = mat[:, :center_x]
-    right = mat[:, center_x:]
-
-    # left = np.hstack((left, np.repeat(np.reshape(left[:, -1], (left.shape[0], 1)), eta, axis=1)))
-    # right = np.hstack((np.repeat(np.reshape(right[:, 0], (right.shape[0], 1)), eta, axis=1), right))
-
-    maskbw_l = np.sum(left < 2, axis=1)  # width of mask black and white area on left side
-    maskbw_r = np.sum(right < 2, axis=1)  # width of mask black and white area on right side
-    maskb_l = np.sum(left == 0, axis=1)  # width of mask black area on left side
-    maskb_r = np.sum(right == 0, axis=1)  # width of mask black area on right side
-    ice_width_l = np.sum(left == 2, axis=1)
-    ice_width_r = np.sum(right == 2, axis=1)
-
-    maskbw_l[ice_width_l == 0] += BIG_NUMBER
-    maskbw_r[ice_width_r == 0] += BIG_NUMBER
-
-    # plt.figure()
-    # plt.plot(widths_l, np.arange(len(widths_l)))
-    # plt.plot(widths_r, np.arange(len(widths_r)))
-
-    r_l = np.nan * np.zeros((left.shape[0], 2 * eta))
-    r_r = np.nan * np.zeros((right.shape[0], 2 * eta))
-
-    r_l[:, 0] = maskbw_l - maskb_l
-    r_r[:, 0] = maskbw_r - maskb_r
-    for i in range(1, eta):
-        r_l[:-i, -i] = np.sqrt((maskbw_l[i:] - maskb_l[:-i]) ** 2 + i ** 2)
-        r_l[i:, i] = np.sqrt((maskbw_l[:-i] - maskb_l[i:]) ** 2 + i ** 2)
-
-        r_r[:-i, -i] = np.sqrt((maskbw_r[i:] - maskb_r[:-i]) ** 2 + i ** 2)
-        r_r[i:, i] = np.sqrt((maskbw_r[:-i] - maskb_r[i:]) ** 2 + i ** 2)
-
-    error_l = np.nanmin(r_l, axis=1) - eta
-    error_r = np.nanmin(r_r, axis=1) - eta
-
-    error_r[error_r > BIG_NUMBER / 2] -= BIG_NUMBER - eta
-    error_l[error_l > BIG_NUMBER / 2] -= BIG_NUMBER - eta
-
-    # plt.figure()
-    # plt.plot(error_l, np.arange(len(error_l)))
-    # plt.plot(error_r, np.arange(len(error_r)))
-
-    new_l = left.copy()
-    new_r = right.copy()
-    for i in range(left.shape[0]):
-        w_l = maskb_l[i] + error_l[i]
-        new_l[i, :int(w_l)] += 3
-
-        w_r = maskb_r[i] + error_r[i]
-        new_r[i, -int(w_r):] += 3
-
-    # plt.figure()
-    # plt.imshow(new_l)
-    # plt.figure()
-    # plt.imshow(new_r)
-
-    fig, ax = plt.subplots(1, 2)
-    ax[0].imshow(new_l)
-    ax[1].imshow(new_r)
-    plt.show()
-
-    # Normalize coordinates
-    L = (bot_y - top_y)  # length scale (height of the mask)
-    y = (np.arange(mat.shape[0]) - top_y) / L
-    error_l = error_l / L
-    error_r = error_r / L
-
-    # plt.figure()
-    # plt.plot(error_l)
-    # plt.plot(error_r)
-    # plt.show()
-    return y, error_l, error_r
 
